5381a9f8ca7bb7e25c963e98e5c038e4f6762877
badripaudel77 <badripaudel77@gmail.com>
2020-10-01 21:03:34 +0545

    react-countup added : number animation

--- src/components/box/Box.js
+++ src/components/box/Box.js
@@ -1,15 +1,21 @@
 import { Card, CardContent, Typography } from '@material-ui/core';
-import React from 'react'
+import React from 'react';
+import CountUp from 'react-countup';
 
 import './Box.css';
 
 export const Box = ({title, cases, total,}) => {
+    console.log('cases ? ', cases);
+    if(!cases || !total|| !title) {
+        return <h2>Data loading...</h2>
+    }
     return (
         <Card className="box">
             <CardContent>
+                {/* <CountUp duration = {1} end ={cases} /> */}
                  <Typography color="textSecondary">{title}</Typography>
-                 <Typography><span>Today +<b>{cases}</b></span></Typography>
-                <Typography>Total{' '}{total}</Typography>
+                 <Typography><span>Today +</span><b><CountUp end={cases} duration={1} /></b></Typography>
+                 <Typography style={{color : 'red'}}><span>Total +</span><CountUp end={total} duration={1} /></Typography>
             </CardContent>
         </Card>
     )
